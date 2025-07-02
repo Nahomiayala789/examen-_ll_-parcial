dd724c033fc7f52e8dbd959a4eb7032b2649a51a
Nahomy Ayala <ruthbardales15@gmail.com>
2025-07-02 09:00:35 -0600

    comienzo de proyecto

---
+++ cliente.java
@@ -0,0 +1,11 @@
+public class cliente {
+    
+    private String  comprador (cliente);
+    private String librocomprado:(libro);{
+
+        public String mostrarResumenString();
+        public calculartotal();
+        public agregarLibro
+    }
+
+}
